d97ea191ddd263949752431b1774821e6fe5c15d
Randallale <randallale@urbandaleschools.com>
2018-12-15 12:24:50 -0600

    Almost done

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -1,39 +1,39 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+but I will definitely not ask another person except the instructor. Signed: Alexander Randall
 
 
 
 1. Write a line of code that will print your name.
 '''
-
+print("Alexander Randall")
 
 '''
 2. How do you enter a comment in a program?
-
 '''
-
+# use a hashtag
 
 '''
 3. What is the ouput of the following lines of code?
 print (2/3)
 print (2//3)
 '''
-
+#the first one is .66
+#the second one is 0
 
 '''
 4. Write a line of code that creates a variable called pi and sets it to an
 appropriate value.
 
 '''
-
+pi = 3.14
 
 '''
 5. Why does this code not work?
 A=22
 print(a)
 '''
-
+# the variable a is not defined but the variable A is
 
 
 '''
@@ -46,7 +46,7 @@ area
 areaOfRectangle
 AreaOfRectangle
 '''
-
+#areaofrectangle because it is specific and doesnt start with a capital
 
 '''
 7. Which of these variables names are not allowed in Python?
@@ -61,7 +61,10 @@ account.number
 accountNumber
 account#
 '''
-
+#1Apple doesnt work
+#account number doesnt work
+#account.number doesnt work
+#account# doesn't work
 
 
 '''
@@ -70,7 +73,7 @@ account#
 print(a)
 a=45
 '''
-
+#this code doesn't work because its asking to print the variable before the variable is defined
 
 
 '''
@@ -78,19 +81,21 @@ a=45
 side and store the result in a variable. Make sure to convert the value
 to an integer.
 '''
-
+side = int(input("What's the length of the square's side?"))
 
 '''
 10. Write a line of code that prints the area of the square, using the
 number the user typed in that you stored in question 9.
 '''
-
+print(side**2)
 
 
 '''11. Do the same as in questions 9 and 10, but with the formula for the
 area of an ellipse. Area=pi*a*b where a and b are the lengths of the major radii.
 '''
-
+a = int(input("What's the length of the radii?"))
+b = int(input("What's the length of the other radii?"))
+print(pi*a*b)
 
 
 '''
