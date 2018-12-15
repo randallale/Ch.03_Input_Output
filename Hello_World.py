3abf81d98f18523157c2c74864ac4c85c5cd5e4f
Randallale <randallale@urbandaleschools.com>
2018-12-14 18:40:12 -0600

    assbutt

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,4 @@
 print("Hello World!")
+print("I want to print a double quote\n \rfor some reason")
+print("Happy Dude", end=' ')
+print("Sad Dude")
---
+++ MPG_calculator.py
@@ -0,0 +1,4 @@
+miles_driven = 294
+gallons_used = 10.5
+mpg = miles_driven / gallons_used
+print(mpg)
