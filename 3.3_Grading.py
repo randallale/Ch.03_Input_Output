58018e3c26506131fac405b505e1aea1f46a406f
Randallale <randallale@urbandaleschools.com>
2018-12-14 22:56:58 -0600

    DOne

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,13 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+print("What's your current semester grade?")
+current = int(input())/100
+print("What was the final exam grade?")
+final_grade = int(input())/100
+print("What was the final's weight")
+weight = int(input())/100
+
+overall = (current*(100-final_grade))/weight
+print(overall)
