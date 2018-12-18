8ae2cb0082127accb0245d94572e961aefcc8c67
randallale <randallale@urbandaleschools.com>
2018-12-18 13:27:30 -0600

    Redoing

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -1,144 +1,61 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: Alexander Randall
-
-
-
+but I will definitely not ask another person except the instructor. Signed: ______________________
 1. Write a line of code that will print your name.
 '''
-print("Alexander Randall")
+
 
 '''
-2. How do you enter a comment in a program?
+2. Write a program that asks someone for their name and then prints their name to the screen?
 '''
-# use a hashtag
+
 
 '''
-3. What is the ouput of the following lines of code?
-print (2/3)
-print (2//3)
+3. Predict the ouput of the following lines of code and then test them? Write down your prediction and the output.
+print (17/9)
+print (17//9)
+print (17%9)
 '''
-#the first one is .66
-#the second one is 0
+
 
 '''
-4. Write a line of code that creates a variable called pi and sets it to an
-appropriate value.
+4. Write a a program where a user enters a base and height and you print the area of a triangle.
+'''
+
 
 '''
-pi = 3.14
-
-'''
-5. Why does this code not work?
-A=22
+5. Change this program so it works.
+A=May the Force be with you!
 print(a)
 '''
-# the variable a is not defined but the variable A is
+
+
 
 
 '''
-6. All of the variable names below can be used. But which of these is the best?
-a
-A
-Area
-AREA
-area
-areaOfRectangle
-AreaOfRectangle
+6. Write a line of code that will ask the user for the length of a square's
+side and then print the area of the square
 '''
-#areaofrectangle because it is specific and doesnt start with a capital
 
+
+
+'''7. Ask a user for the length of radii of an ellipse and then print its area. 
+Area=pi*a*b where a and b are the lengths of the major radii.
 '''
-7. Which of these variables names are not allowed in Python?
-apple
-Apple
-APPLE
-Apple2
-1Apple
-account number
-account_number
-account.number
-accountNumber
-account#
-'''
-#1Apple doesnt work
-#account number doesnt work
-#account.number doesnt work
-#account# doesn't work
+
 
 
 '''
-8. Why does this code not work?
-
-print(a)
-a=45
-'''
-#this code doesn't work because its asking to print the variable before the variable is defined
-
-
-'''
-9. Write a line of code that will ask the user for the length of a square's
-side and store the result in a variable. Make sure to convert the value
-to an integer.
-'''
-side = int(input("What's the length of the square's side?"))
-
-'''
-10. Write a line of code that prints the area of the square, using the
-number the user typed in that you stored in question 9.
-'''
-print(side**2)
-
-
-'''11. Do the same as in questions 9 and 10, but with the formula for the
-area of an ellipse. Area=pi*a*b where a and b are the lengths of the major radii.
-'''
-a = int(input("What's the length of the radii?"))
-b = int(input("What's the length of the other radii?"))
-print(pi*a*b)
-
-
-'''
-12. Do the same as in questions 9 and 10, but with a formula to find
-the pressure of a gas. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
+8. Ask a user for moles, volume and temperature of a gas and print out the pressure. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
 volume, and R is the gas constant 8.3144.
 '''
-n = int(input("What's are the moles?"))
-r = 8.3144
-t = int(input("What's the absolute temperature?"))
-v = int(input("What's the volume?"))
-p = (n*r*t)/v
-print(p)
 
 '''
-13. Explain the mistake in this code:
-
-pi = float(3.14)
+9. Ask a user for an integer and then print the square root.
 '''
-# you don't need the float because it would already be an integer
 
 
 '''
-14. Assume radius has been defined. This code runs, but isn't very good. Explain the mistake in the following code:
-
-x=3.14
-pi=x
-area=pi*radius**2
+10. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. Ask the user for mass and acceleration
+and then print out the Force on one line and "Get it?" on the next.
 '''
-# you should just do pi = 3.14 instead of the whole x thing
-
-'''
-Assuming x and y are already defined, this code runs. But
-something isn't quite right. Explain the mistake in the following code:
-
-a=((x)*(y))
-'''
-# You should just do a=(x*y) there is no need for the extra set of () beside the variables
-
-'''
-16. Explain the mistake in the following code:
-
-radius = input(float("Enter the radius:"))
-'''
-# the float and the input should be switched around
-
