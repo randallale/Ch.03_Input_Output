6043fd414b5dbb809c25bc53a9b736a5c0055647
randallale <randallale@urbandaleschools.com>
2018-12-18 13:39:23 -0600

    Redoing

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -3,12 +3,12 @@ HONOR CODE: I solemnly promise that while taking this test I will only use PyCha
 but I will definitely not ask another person except the instructor. Signed: ______________________
 1. Write a line of code that will print your name.
 '''
-
+print("Alex")
 
 '''
 2. Write a program that asks someone for their name and then prints their name to the screen?
 '''
-
+print(input("What's your name? "))
 
 '''
 3. Predict the ouput of the following lines of code and then test them? Write down your prediction and the output.
@@ -16,28 +16,29 @@ print (17/9)
 print (17//9)
 print (17%9)
 '''
-
+# I predict that the first one would do 17/9 OUTPUT: 1.888
+# I predict the second one will do 17/9 then floor the answer OUTPUT: 1
+# The third one would do the remainder of 17/9 OUTPUT: 8
 
 '''
 4. Write a a program where a user enters a base and height and you print the area of a triangle.
 '''
-
-
+b = int(input("Base:  \n"))
+h = int(input("Height:  \n"))
+print("The area of the triangle is", ((h*b)/2))
 '''
 5. Change this program so it works.
 A=May the Force be with you!
 print(a)
 '''
-
-
-
-
+a=("May the Force be with you!")
+print(a)
 '''
 6. Write a line of code that will ask the user for the length of a square's
 side and then print the area of the square
 '''
-
-
+side = int(input("What's the length of the square's side? \n"))
+print("The area is", ())
 
 '''7. Ask a user for the length of radii of an ellipse and then print its area. 
 Area=pi*a*b where a and b are the lengths of the major radii.
